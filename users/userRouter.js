3882ccae7adb0c07ce2a38c6236ae6eff3af39a1
chikechris <cchike2017@gmail.com>
2019-09-05 14:56:14 -0400

    added dotenv

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,4 @@
-const express =require( 'express')
+const express = require('express')
 const usersDb = require('./userDb.js')
 
 const router = express.Router()
@@ -46,79 +46,81 @@ router.get('/:id/posts', validateUserId, (req, res) => {
   usersDb
     .getUserPosts(req.params.id)
     .then(userPosts => {
-      res.status(200).json(userPosts);
+      res.status(200).json(userPosts)
     })
     .catch(err => {
-      res.status(500).json(err);
-    });
-});
+      res.status(500).json(err)
+    })
+})
 
 router.delete('/:id', validatePostId, (req, res) => {
   postsDB
     .remove(req.params.id)
     .then(removed => {
-      res.status(200).json({ message: 'The post has been removed' });
+      res.status(200).json({ message: 'The post has been removed' })
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
+
+
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+  usersDb
+    .update(req.params.id, req.body)
+    .then(user => {
+      res.status(201).json(user);
     })
     .catch(err => {
       res.status(500).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {})
-
 // custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId (req, res, next) {
   usersDb
     .getById(req.params.id)
     .then(user => {
       if (user) {
-        req.user = user;
-     
-        next();
+        req.user = user
+
+        next()
       } else {
-        res.status(404).json({ message: 'Id not found' });
+        res.status(404).json({ message: 'Id not found' })
       }
     })
     .catch(err => {
-      res.status(500).json(err);
-    });
+      res.status(500).json(err)
+    })
 }
 
-function validateUser(req, res, next) {
-  console.log(Object.keys(req.body));
+function validateUser (req, res, next) {
+  console.log(Object.keys(req.body))
   if (Object.keys(req.body).length === 0) {
-    res.status(400).json({ message: 'missing user data.' });
+    res.status(400).json({ message: 'missing user data.' })
   } else if (!req.body.name) {
-    res.status(400).json({ message: 'missing required name field' });
+    res.status(400).json({ message: 'missing required name field' })
   } else {
-    next();
+    next()
   }
-  
 }
 
-function validatePostId(req, res, next) {
+function validatePostId (req, res, next) {
   postsDB
     .getById(req.params.id)
     .then(post => {
-      console.log(post); // { id: 9, text: 'Well, that rules you out.', user_id: 3 }
+      console.log(post) // { id: 9, text: 'Well, that rules you out.', user_id: 3 }
       if (post) {
-        req.post = post;
-        next();
+        req.post = post
+        next()
       } else {
-        res.status(404).json({ message: 'Id not found' });
+        res.status(404).json({ message: 'Id not found' })
       }
     })
     .catch(err => {
-      res.status(500).json(err);
-    });
+      res.status(500).json(err)
+    })
 }
-
-
-
 
 module.exports = router
